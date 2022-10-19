23c83bc61241aa1737d861f262fcb136ea2a999a
Ramesh Ejjagiri <ramesh.ec28@gmail.com>
2022-10-19 14:08:45 +0530

    endpoints added

--- company-register/src/main/java/com/company/register/CompanyRegisterApplication.java
+++ company-register/src/main/java/com/company/register/CompanyRegisterApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@OpenAPIDefinition(info = @Info(title = "EStock Company API", version = "1.0", description = "Company Information"))
 public class CompanyRegisterApplication {
 
 	public static void main(String[] args) {
--- company-register/src/main/java/com/company/register/model/EStockCompany.java
+++ company-register/src/main/java/com/company/register/model/EStockCompany.java
@@ -1,6 +1,9 @@
 package com.company.register.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,7 +18,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EStockCompany {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer companyCode;
 	private String companyName;
 	private String companyCEO;
